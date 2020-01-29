6b41dd08dad044a7f010b02495e36336d22d12c9
cypher-black <57178922+cypher-black@users.noreply.github.com>
2020-01-29 11:00:51 +0530

    Create guess_num.py

---
+++ guess_num.py
@@ -0,0 +1,15 @@
+import random
+n=random.randint(0,99)
+print(n)
+g=int(input("your guess"))
+while n != "find":
+    if g<n:
+        print("guess is low")
+        g=int(input("your guess"))
+    elif g>n:
+        print("guess is high")
+        g=int(input("your guess"))
+    elif g==n:
+        print("your guess is right")
+        break
+    
